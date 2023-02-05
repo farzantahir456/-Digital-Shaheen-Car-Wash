252a49720ddd2bfdcbf566a53ccea2540098001d
ilyas yaqoob <ilyasyaqoob@gmail.com>
2023-02-05 12:39:12 +0500

    updated

--- src/routes/admin/admin_Routes.js
+++ src/routes/admin/admin_Routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const adminRoute = express.Router();
 
-const { getworker, getcustomer,updateadminprofile} =require('../../controllers/admin-controller/admin')
+const { getworker, getcustomer} =require('../../controllers/admin-controller/admin-controller')
 
 
 adminRoute.get("/", getworker);
 adminRoute.get("/customer", getcustomer);
-adminRoute.put("/:id", updateadminprofile);
+// adminRoute.put("/:id", updateadminprofile);
 module.exports= adminRoute ;
