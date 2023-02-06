c03618625f21972639d423d08bbde1239ff2c339
ilyas yaqoob <ilyasyaqoob@gmail.com>
2023-02-06 11:32:06 +0500

    updated

--- src/controllers/admin-controller/admin-controller.js
+++ src/controllers/admin-controller/admin-controller.js
@@ -13,8 +13,8 @@ const getworker = (req, res) => {
   };
 
 
-  const getcustomer = (req, res) => {
-    pool.query(`SELECT * FROM customers`, (err, results) => {
+  const appointments = (req, res) => {
+    pool.query(`SELECT * FROM appointments`, (err, results) => {
       if (err) {
         throw err;
       }
@@ -24,7 +24,6 @@ const getworker = (req, res) => {
 
   const getappointment=(req,res)=>{
     pool.query (`SELECT customer_name, worker_name, service_type from appointments
-    JOIN customers on appointments.customer_id = customers.customer_id
     JOIN workers on appointments.worker_id = workers.worker_id
     JOIN services on appointments.service_id = services.service_id  `,(err,results)=>{
       if(err){
@@ -48,7 +47,7 @@ const getworker = (req, res) => {
   module.exports = {
     getworker, 
     // updateadminprofile,
-    getcustomer,
+    appointments,
     getappointment
     
   };
--- src/routes/admin/admin_Routes.js
+++ src/routes/admin/admin_Routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const adminRoute = express.Router();
 
-const { getworker, getcustomer,getappointment} =require('../../controllers/admin-controller/admin-controller')
+const { getworker,getappointment, appointments} =require('../../controllers/admin-controller/admin-controller')
 const auth=require("../../middleware/admin/admin-auth")
 
 
 adminRoute.get("/worker", auth,getworker);
-adminRoute.get("/customer", auth,getcustomer);
+adminRoute.get("/", auth,appointments);
 adminRoute.get("/appointment",auth,getappointment)
 // adminRoute.put("/:id", updateadminprofile);
 // 
