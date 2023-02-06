9065ebff965ae8af56d193fafcd6b03252d4534e
farzan <farzan@gamil.com>
2023-02-06 12:48:40 +0500

    conflicts resolved

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
+
 const app = express();
+
+
 require('dotenv').config()
+const port = process.env.WEB_PORT;
 const userRoute=require('./src/routes/worker/authWorkerRoutes')
 const adminRoute=require('./src/routes/admin/admin_Routes')
 const admin=require('./src/routes/admin/authAdminRoutes')
+const customerRoute=require('./src/routes/customer/customer-routes')
 app.use(express.json());
 app.use('/worker', userRoute);
 app.use('/admin', adminRoute);
 app.use('/adminlogin',admin)
-const port = process.env.WEB_PORT;
+app.use('/customer',customerRoute)
 
 
 
